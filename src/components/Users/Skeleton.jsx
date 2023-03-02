e2b6b77a4c35ae38cb27cdd51e9d78e2f2aa3cc4
garich94 <114189622+garich94@users.noreply.github.com>
2023-03-03 00:28:04 +0300

    первые изменения

--- src/components/Users/Skeleton.jsx
+++ src/components/Users/Skeleton.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import ContentLoader from 'react-content-loader';
+import React from "react";
+import ContentLoader from "react-content-loader";
 
 export const Skeleton = () => (
   <ContentLoader
@@ -8,7 +8,8 @@ export const Skeleton = () => (
     height={50}
     viewBox="0 0 320 50"
     backgroundColor="#f3f3f3"
-    foregroundColor="#ecebeb">
+    foregroundColor="#ecebeb"
+  >
     <circle cx="25" cy="25" r="25" />
     <rect x="64" y="0" rx="6" ry="6" width="137" height="25" />
     <rect x="64" y="32" rx="6" ry="6" width="183" height="15" />
